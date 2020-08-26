8e1be53711a8a60c192b4b54093ef1ee95f2240b
UpperCod <mtrujillo@outlook.cl>
2020-08-26 15:45:53 -0400

    export types  (ts)

--- types.d.ts
+++ types.d.ts
@@ -1,22 +1,22 @@
-interface Value {
-    imported: string[];
-    root?: boolean;
-}
-
-interface Tree {
-    [index: string]: Value;
-}
-
-interface Context {
-    tree: Tree;
-    has(str: string): boolean;
-    get(str: string): Value;
-    add(src: string): void;
-    addChild(src: string, childSrc: string): void;
-    getParents(src: string, childSrc: string): void;
-    remove(src: string): void;
-}
-
 declare module "@uppercod/imported" {
+    export interface Value {
+        imported: string[];
+        root?: boolean;
+    }
+
+    export interface Tree {
+        [index: string]: Value;
+    }
+
+    export interface Context {
+        tree: Tree;
+        has(str: string): boolean;
+        get(str: string): Value;
+        add(src: string): void;
+        addChild(src: string, childSrc: string): void;
+        getParents(src: string, childSrc: string): void;
+        remove(src: string): void;
+    }
+
     export default function createTree(tree?: Tree): Context;
 }
